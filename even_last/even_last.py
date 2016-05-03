6b7f87ef6a45a77b9c7b245f7f71023e4c7031e2
Mehdi Ouaghlani <mouaghlani@octo.com>
2016-05-03 10:22:22 +0200

    Ajouts autres premiers checkio

---
+++ even_last/even_last.py
@@ -0,0 +1,26 @@
+array = []
+last_number = 0
+result = 0
+
+
+def checkio(array):
+    sum = 0
+    if array <> []:
+	last_number = array[-1]
+        size = len(array)
+        for i in range(0, size , 2):
+            sum += array[i]
+        result = sum * last_number   
+    else:
+	result = 0
+
+    return result
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
+    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
+    assert checkio([6]) == 36, "(6)*6=36"
+    assert checkio([]) == 0, "An empty array = 0"
+    print("ok")
+
--- non_unique_element/non_unique_element.py
+++ non_unique_element/non_unique_element.py
@@ -23,4 +23,5 @@ if __name__ == "__main__":
     assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
     assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
     assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
+    print('ok')
 
